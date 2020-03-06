da3d3fcda2792cc6bc521d76bbbd71206168dc39
Jørgen <mail@jvik.no>
2020-03-07 00:00:40 +0100

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import axios from "axios";
 import VueApexCharts from "vue-apexcharts";
+import vuetify from "./plugins/vuetify";
 
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.use(VueApexCharts);
 Vue.component("apexchart", VueApexCharts);
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
