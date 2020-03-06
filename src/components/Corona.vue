18053badfde1913b5f86d4a2f30a1150e615c696
Jørgen <mail@jvik.no>
2020-03-06 23:43:14 +0100

    Renamed field

--- src/components/Corona.vue
+++ src/components/Corona.vue
@@ -37,7 +37,7 @@ export default {
           id: "vuechart-example"
         },
         xaxis: {
-          categories: ["Sykdom", "Dødsfall"]
+          categories: ["Bekreftet smittet", "Dødsfall"]
         }
       }
     };
