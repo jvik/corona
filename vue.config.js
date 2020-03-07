07f72ed6404a61c4fcb51c19ed45e20124e360e1
Jørgen <mail@jvik.no>
2020-03-07 01:39:07 +0100

    Reverting pwa options and trying to add manifest file

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,3 @@
 module.exports = {
 	transpileDependencies: ["vuetify"],
-	pwa: {
-    name: 'Corona',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black-translucent',
-
-   // configure the manifest options
-   manifestOptions: {
-     display: 'portrait',
-     background_color: "#42B883"
-     // ...
-   },
-
-    // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/registerServiceWorker.js',
-      // ...other Workbox options...
-    }
-  }
 };
