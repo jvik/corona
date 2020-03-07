38c2150324f8289ea0eacd4e8905004baf0d1f4c
Jørgen <mail@jvik.no>
2020-03-07 01:32:30 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+	transpileDependencies: ["vuetify"],
+	pwa: {
+    name: 'Corona',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+
+   // configure the manifest options
+   manifestOptions: {
+     display: 'portrait',
+     background_color: "#42B883"
+     // ...
+   },
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 };
