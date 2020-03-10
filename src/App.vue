be04d5efd2e21c50316c1bcaa1efd366adb2ec3c
Jørgen <mail@jvik.no>
2020-03-10 20:48:35 +0100

    Added prettier

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <v-app>
-    <Corona msg="Norsk Coronastatistikk (COVID-19)" />
-  </v-app>
+    <v-app>
+        <Corona msg="Norsk Coronastatistikk (COVID-19)" />
+    </v-app>
 </template>
 
 <script>
-import Corona from "./components/Corona.vue";
+import Corona from './components/Corona.vue'
 
 export default {
-  name: "App",
-  components: {
-    Corona
-  }
-};
+    name: 'App',
+    components: {
+        Corona,
+    },
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
